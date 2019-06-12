045693b177e18852921e458eb273fd25c0f7cfb6
Anthony <anthonyballugjr@gmail.com>
2019-06-12 20:53:56 +0800

    optimized sc

--- config/database.php
+++ config/database.php
@@ -2,34 +2,32 @@
 session_start();
 
 class Database{
-
-  public $conn;
-
-  public function getConnection(){
-   
-    // $host = "PHBG1SVFFIX";
-        $host = "ANTHONY\SQLEXPRESS";
-    $username = "sa";
-    // $password = "F&fadmin01";
-        $password = "jampong19";
-    $db_name = "prq";
-
+    
+    public $conn;
+    
+    public function getConnection(){
+        
+        $host = "SQLEXPRESS";
+        $username = "sa";
+        $password = "password";
+        $db_name = "prq";
+        
         // $host = "localhost";
         // $username = "root";
         // $password = "";
         // $db_name = "prq";
-
-    $this->conn = null;
-    try {
-          // $this->conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-      $this->conn = new PDO("sqlsrv:Server=$host;Database=$db_name",$username, $password);
-      $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $this->conn = null;
+        try {
+            // $this->conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password); 
+            $this->conn = new PDO("sqlsrv:Server=$host;Database=$db_name",$username, $password);
+            $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo "Connection Established!";
-    } catch (PDOException $exception) {
-      echo "Database Connection Error!" . $exception->getMessage();
+        } catch (PDOException $exception) {
+            echo "Database Connection Error!" . $exception->getMessage();
+        }
+        return $this->conn;
     }
-    return $this->conn;
-  }
-
+    
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,8 @@
 # PARTS REPLACEMENT REQUEST WITH LDAP AUTHENTICATION
 
 ## Built With
-
-* [XAMPP stack](https://www.apachefriends.org/index.html)
-* [HTML5]()
-* [CSS3]()
+* [PHP 7.2.0](hhttps://php.net/)
+* SQL/MYSQL
 
 ## Plugins/Libraries/Frameworks Used
 
@@ -13,3 +11,14 @@
 * [DataTables](https://datatables.net/download/#dt/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-html5-1.5.4/b-print-1.5.4)
 * [Font Awesome](https://fontawesome.com/)
 * [Select2](https://select2.org/)
+
+## Database Server
+* [MS SQL SERVER](https://www.microsoft.com/en-us/sql-server/sql-server-2017)
+
+## Runtime
+* [XAMPP](https://www.apachefriends.org/)
+
+# Additional Information
+* Make sure you have PDO SQL Driver Installed if you want to make use of MSSQL/SQL Server.
+* If you are going to use MYSQL, you might want to switch between the queries or alter them on the [models/classes](https://github.com/anthonyballugjr/part-request/tree/master/classes) for them to function because of some SQL and MYSQL syntax differences (e.g. TOP (10) vs LIMIT 10).
+* Helpful Migration Tools [Microsoft SQL Server Migration Assistant for MySQL](https://www.microsoft.com/en-us/download/details.aspx?id=54257), [MYSQL Workbench](https://www.mysql.com/products/workbench/)
